61c33d99e4d42667eed6ee2a23ed9f7ef1166abe
8717860914 <shvyas@bestpeers.com>
2023-02-13 12:54:24 +0530

    Modified

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,43 +1,43 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
-    respond_to do |format|
-      format.html
-      format.csv { send_data Item.to_csv, filename: "items-#{DateTime.now.strftime("%d%m%Y%H%M")}.csv"}
-    end
+      @items = Item.all
+      respond_to do |format|
+        format.html
+        format.csv { send_data Item.to_csv, filename: "items-#{DateTime.now.strftime("%d%m%Y%H%M")}.csv"}
+      end
   end 
   def new
-   @item = Item.new
+      @item = Item.new
   end
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      # CrudNotificationMailer.create_notification(@item).deliver_later
-      redirect_to users_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+      @item = Item.new(item_params)
+      if @item.save
+        # CrudNotificationMailer.create_notification(@item).deliver_later
+        redirect_to users_path
+      else
+        render :new, status: :unprocessable_entity
+      end
   end  
   def destroy
-    @item = Item.find(params[:id])
-    @item.delete
-    # CrudNotificationMailer.delete_notification(@item). deliver_now
-    redirect_to(controller: :users, action: :show)
+      @item = Item.find(params[:id])
+      @item.delete
+      # CrudNotificationMailer.delete_notification(@item). deliver_now
+      redirect_to(controller: :users, action: :show)
   end
   def edit
-    @item = Item.find(params[:id])
+      @item = Item.find(params[:id])
   end
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      # CrudNotificationMailer.update_notification(@item).deliver_later
-      redirect_to(controller: :users, action: :show)
-    else
-      render :edit, status: :unprocessable_entity
-    end
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        # CrudNotificationMailer.update_notification(@item).deliver_later
+        redirect_to(controller: :users, action: :show)
+      else
+        render :edit, status: :unprocessable_entity
+      end
   end
   private
-  def item_params
-    params.require(:item).permit(:name, :price, :rating)
-  end
+      def item_params
+        params.require(:item).permit(:name, :price, :rating)
+      end
 end     
