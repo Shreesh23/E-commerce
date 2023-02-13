880af4b2698d47dfeddbafc2024cba3d0021bc67
8717860914 <shvyas@bestpeers.com>
2023-02-13 13:01:04 +0530

    Modified

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,43 +1,43 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.all
-    respond_to do |format|
+    def index
+      @items = Item.all
+      respond_to do |format|
       format.html
       format.csv { send_data Item.to_csv, filename: "items-#{DateTime.now.strftime("%d%m%Y%H%M")}.csv"}
+      end
+    end 
+    def new
+    @item = Item.new
     end
-  end 
-  def new
-   @item = Item.new
-  end
-  def create
-    @item = Item.new(item_params)
-    if @item.save
+    def create
+      @item = Item.new(item_params)
+      if @item.save
       # CrudNotificationMailer.create_notification(@item).deliver_later
       redirect_to users_path
-    else
+      else
       render :new, status: :unprocessable_entity
+      end
+    end  
+    def destroy
+      @item = Item.find(params[:id])
+      @item.delete
+      # CrudNotificationMailer.delete_notification(@item). deliver_now
+      redirect_to(controller: :users, action: :show)
     end
-  end  
-  def destroy
-    @item = Item.find(params[:id])
-    @item.delete
-    # CrudNotificationMailer.delete_notification(@item). deliver_now
-    redirect_to(controller: :users, action: :show)
-  end
-  def edit
-    @item = Item.find(params[:id])
-  end
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
+    def edit
+      @item = Item.find(params[:id])
+    end
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
       # CrudNotificationMailer.update_notification(@item).deliver_later
       redirect_to(controller: :users, action: :show)
-    else
+      else
       render :edit, status: :unprocessable_entity
+      end
     end
-  end
   private
-  def item_params
+    def item_params
     params.require(:item).permit(:name, :price, :rating)
-  end
+    end
 end    
